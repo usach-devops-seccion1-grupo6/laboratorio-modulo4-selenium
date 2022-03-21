b8d7e3017a9480735a011a01c4666df87b0ae322
Eduardo Cabrera <lalo@eof.cl>
2022-03-21 01:38:59 -0300

    primer commit

---
+++ src/test/java/selenium/AppTest.java
@@ -0,0 +1,36 @@
+package selenium;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.Test;
+import org.junit.Before;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test1() {
+    }
+
+    @Test
+    public void test2() {
+    }
+}
